f8918a8aabfb876ca897c1dc367ced72d51e9956
jiyongp <jiyongp@google.com>
2021-02-25 07:16:35 -0800

    Move some qcom libraries to vendor partition
    SoC-specific libraries must go to vendor partition so that we can build
    a generic system image that runs on multiple devices.
    
    For now, graphics libraries for msm8996 are moved. Ideally, almost
    everything under /hardware/qcom will be moved to vendor.
    
    Bug: 34826389
    Bug: 35232083
    Test: build & run any msm8994 based device (such as angler)
    Change-Id: Ic089584186e4c74dfdc2c3af4bc7d2777b084828

--- msm8994/libqdutils/Android.mk
+++ msm8994/libqdutils/Android.mk
@@ -3,6 +3,7 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
+LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_HEADER_LIBRARIES        := libhardware_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
@@ -29,5 +30,6 @@ LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\" -Wno-sign-conversion
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libqdMetaData
+LOCAL_PROPRIETARY_MODULE        := true
 include $(BUILD_SHARED_LIBRARY)
 
